7341ddddfe19208ff7167df9991f89003a8f6a88
Cenk Aktaş <cnkakts77@gmail.com>
2026-05-09 22:50:28 +0300

    Dark Ninja indirme linki güncellendi

--- src/pages/DarkNinja.jsx
+++ src/pages/DarkNinja.jsx
@@ -92,7 +92,7 @@ export default function DarkNinja() {
         <div className="download-section">
           <h2>İndir</h2>
           <p>Windows için hemen indirin ve maceraya başlayın!</p>
-          <a href="#" className="download-btn windows-btn">
+          <a href="/DarkNinja.zip" download className="download-btn windows-btn">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
               <path d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.9-1.801" />
             </svg>
